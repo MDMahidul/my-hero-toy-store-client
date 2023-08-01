0ab1f5d712977fb534176a5fa82f50ad5e752b2a
MDMahidul <mmbmahidul007@gmail.com>
2023-08-01 21:01:45 +0600

    created react app and install dependencies and created few pages

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="text-6xl font-bold">
+        MY HERO
+      </p>
+    </>
+  )
+}
+
+export default App

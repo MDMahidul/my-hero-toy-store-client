68ee76146ce69b32c1413821fca74fed81b27f61
MDMahidul <mmbmahidul007@gmail.com>
2023-08-03 02:27:16 +0600

    added search field to the all toys page and showed data by serch by toy name

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -3,12 +3,56 @@ import { Link, useLoaderData, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../Provider/AuthProvider';
 
 const AllToys = () => {
-    const toys = useLoaderData();
+    const loadedToys = useLoaderData();
+    const [toys,setToys]=useState(loadedToys);
+
+     const handleSearch = (e) => {
+      e.preventDefault();
+      const form = e.target;
+      const toyName = form.toyName.value;
+       fetch(`http://localhost:5000/byname/${toyName}`)
+         .then((res) => res.json())
+         .then((data) => {
+           console.log(data);
+           setToys(data);
+         });
+     };
     return (
       <div className="container mx-auto px-4 mt-5 mb-20">
         <h1 className="text-center py-4 font-bold text-red-600 text-3xl">
           All Toys
         </h1>
+        <div className="flex justify-center mb-4">
+          <form onSubmit={handleSearch}>
+            <div className="form-control">
+              <div className="input-group">
+                <input
+                  type="text"
+                  placeholder="Search By Name"
+                  className="input input-bordered"
+                  name="toyName"
+                />
+                <button className="btn btn-square">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+
         <div className="overflow-x-auto ">
           <table className="table">
             {/* head */}

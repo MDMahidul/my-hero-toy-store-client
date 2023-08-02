82647e692534716e4c8e2b9a6206215ee7588d28
MDMahidul <mmbmahidul007@gmail.com>
2023-08-02 11:31:20 +0600

    created context api and set it for the whole route

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,12 +1,65 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../firebase/firebase.config';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-const AuthProvider = () => {
+const AuthProvider = ({children}) => {
+    const [user,setUser] = useState(null);
+    const [loading,setLoading] =  useState(true);
+
+    const userSignup = (email,password)=>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+
+    //sign in using google acc
+    const googleSignin= ()=>{
+        setLoading(true);
+        return signInWithPopup(auth,googleProvider);
+    }
+
+    const userSignin = (email,password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+
+    const userLogout = ()=>{
+        setLoading(true);
+        return signOut(auth)
+    }
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth,(loggedUser)=>{
+            setUser(loggedUser);
+            setLoading(false);
+        });
+        return ()=>{
+           return unsubscribe();
+        }
+    },[])
+
+    const authInfo = {
+      userSignup,
+      googleSignin,
+      userSignin,
+      userLogout,
+      user,loading
+    };
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
